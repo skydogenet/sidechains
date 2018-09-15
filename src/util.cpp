4feba7ed64dd42efdcbcb0b5ea536c36b0f6c936
CryptAxe <cryptaxe@gmail.com>
2018-09-14 21:32:39 -0700

    Update configuration file & pid file names

--- src/util.cpp
+++ src/util.cpp
@@ -83,8 +83,8 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoind.pid";
+const char * const BITCOIN_CONF_FILENAME = "sidechain.conf";
+const char * const BITCOIN_PID_FILENAME = "sidechain.pid";
 const char * const DEFAULT_DEBUGLOGFILE = "debug.log";
 
 ArgsManager gArgs;
