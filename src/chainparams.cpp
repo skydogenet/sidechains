d45ca93c30f3f2f215d6068816d11eb17dcb160e
CryptAxe <cryptaxe@gmail.com>
2018-09-29 12:56:54 -0700

    Change network magic bytes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -116,10 +116,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf9;
-        pchMessageStart[1] = 0xbe;
-        pchMessageStart[2] = 0xb4;
-        pchMessageStart[3] = 0xd9;
+        pchMessageStart[0] = 0xd9;
+        pchMessageStart[1] = 0xb4;
+        pchMessageStart[2] = 0xbe;
+        pchMessageStart[3] = 0xf9;
         nDefaultPort = 2333;
         nPruneAfterHeight = 100000;
 
