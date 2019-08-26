43bd9caf5f9d50148fbd868b3a4c7aa54da6100b
CryptAxe <cryptaxe@gmail.com>
2019-08-25 23:09:09 -0700

    Add CorePolicy class with core TX standards

---
+++ src/policy/corepolicy.cpp
@@ -0,0 +1,128 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <policy/corepolicy.h>
+
+// TODO the following includes could be replaced with the core versions.
+#include <amount.h>
+#include <policy/fees.h>
+#include <primitives/transaction.h>
+#include <script/script.h>
+#include <serialize.h>
+#include <consensus/validation.h>
+
+CAmount CoreGetDustThreshold(const CTxOut& txout, const CFeeRate& dustRelayFeeIn)
+{
+    if (txout.scriptPubKey.IsUnspendable())
+        return 0;
+
+    size_t nSize = GetSerializeSize(txout, SER_DISK, 0);
+    int witnessversion = 0;
+    std::vector<unsigned char> witnessprogram;
+
+    if (txout.scriptPubKey.IsWitnessProgram(witnessversion, witnessprogram)) {
+        // sum the sizes of the parts of a transaction input
+        // with 75% segwit discount applied to the script size.
+        nSize += (32 + 4 + 1 + (107 / CORE_WITNESS_SCALE_FACTOR) + 4);
+    } else {
+        nSize += (32 + 4 + 1 + 107 + 4); // the 148 mentioned above
+    }
+
+    return dustRelayFeeIn.GetFee(nSize);
+}
+
+bool CoreIsDust(const CTxOut& txout, const CFeeRate& dustRelayFeeIn)
+{
+    return (txout.nValue < CoreGetDustThreshold(txout, dustRelayFeeIn));
+}
+
+bool CoreIsStandard(const CScript& scriptPubKey, txnouttype& whichType)
+{
+    // TODO should use the core version of solver, and core txnouttype(s)
+    std::vector<std::vector<unsigned char> > vSolutions;
+    if (!Solver(scriptPubKey, whichType, vSolutions))
+        return false;
+
+    if (whichType == TX_NONSTANDARD) {
+        return false;
+    } else if (whichType == TX_MULTISIG) {
+        unsigned char m = vSolutions.front()[0];
+        unsigned char n = vSolutions.back()[0];
+        // Support up to x-of-3 multisig txns as standard
+        if (n < 1 || n > 3)
+            return false;
+        if (m < 1 || m > n)
+            return false;
+    } else if (whichType == TX_NULL_DATA &&
+               (!fAcceptDatacarrier || scriptPubKey.size() > nMaxDatacarrierBytes)) {
+          return false;
+    }
+
+    return true;
+}
+
+bool CoreIsStandardTx(const CTransaction& tx, bool permit_bare_multisig, const CFeeRate& dust_relay_fee, std::string& reason)
+{
+    if (tx.nVersion > CORE_MAX_STANDARD_TX_VERSION || tx.nVersion < 1) {
+        reason = "version";
+        return false;
+    }
+
+    // Extremely large transactions with lots of inputs can cost the network
+    // almost as much to process as they cost the sender in fees, because
+    // computing signature hashes is O(ninputs*txsize). Limiting transactions
+    // to MAX_STANDARD_TX_WEIGHT mitigates CPU exhaustion attacks.
+    unsigned int sz = GetTransactionWeight(tx); // TODO use the core version
+    if (sz > CORE_MAX_STANDARD_TX_WEIGHT) {
+        reason = "tx-size";
+        return false;
+    }
+
+    for (const CTxIn& txin : tx.vin)
+    {
+        // Biggest 'standard' txin is a 15-of-15 P2SH multisig with compressed
+        // keys (remember the 520 byte limit on redeemScript size). That works
+        // out to a (15*(33+1))+3=513 byte redeemScript, 513+1+15*(73+1)+3=1627
+        // bytes of scriptSig, which we round off to 1650 bytes for some minor
+        // future-proofing. That's also enough to spend a 20-of-20
+        // CHECKMULTISIG scriptPubKey, though such a scriptPubKey is not
+        // considered standard.
+        if (txin.scriptSig.size() > 1650) {
+            reason = "scriptsig-size";
+            return false;
+        }
+        if (!txin.scriptSig.IsPushOnly()) {
+            reason = "scriptsig-not-pushonly";
+            return false;
+        }
+    }
+
+    unsigned int nDataOut = 0;
+    txnouttype whichType;
+    for (const CTxOut& txout : tx.vout) {
+        if (!CoreIsStandard(txout.scriptPubKey, whichType)) {
+            reason = "scriptpubkey";
+            return false;
+        }
+
+        if (whichType == TX_NULL_DATA)
+            nDataOut++;
+        else if ((whichType == TX_MULTISIG) && (!permit_bare_multisig)) {
+            reason = "bare-multisig";
+            return false;
+        } else if (CoreIsDust(txout, dust_relay_fee)) {
+            reason = "dust";
+            return false;
+        }
+    }
+
+    // only one OP_RETURN txout is permitted
+    if (nDataOut > 1) {
+        reason = "multi-op-return";
+        return false;
+    }
+
+    return true;
+}
---
+++ src/policy/corepolicy.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_CORE_POLICY_H
+#define BITCOIN_CORE_POLICY_H
+
+#include <script/standard.h>
+#include <string>
+#include <stdlib.h>
+#include <stdint.h>
+
+class CFeeRate;
+class CScript;
+class CTransaction;
+
+/**
+ * This class exists so that WT^(s) can be checked for most mainchain policies
+ * and standardness requirements without contacting the mainchain.
+ *
+ * Bitcoin core policy settings, taken from github.com/bitcoin/bitcoin
+ * policy/policy.h, primitives/transaction.h, consensus/consensus.h
+ */
+
+static const int CORE_WITNESS_SCALE_FACTOR = 4;
+
+// TODO check when creating WT^
+//static const size_t CORE_MIN_TRANSACTION_WEIGHT = CORE_WITNESS_SCALE_FACTOR * 60; // 60 is the lower bound for the size of a valid serialized CTransaction
+//static const size_t CORE_MIN_SERIALIZABLE_TRANSACTION_WEIGHT = CORE_WITNESS_SCALE_FACTOR * 10; // 10 is the lower bound for the size of a serialized CTransaction
+
+/** The maximum weight for transactions we're willing to relay/mine */
+static const unsigned int CORE_MAX_STANDARD_TX_WEIGHT = 400000;
+
+// TODO check when creating WT^
+/** The minimum non-witness size for transactions we're willing to relay/mine (1 segwit input + 1 P2WPKH output = 82 bytes) */
+//static const unsigned int CORE_MIN_STANDARD_TX_NONWITNESS_SIZE = 82;
+/** The maximum number of sigops we're willing to relay/mine in a single tx */
+//static const unsigned int CORE_MAX_STANDARD_TX_SIGOPS_COST = CORE_MAX_BLOCK_SIGOPS_COST/5;
+/** Min feerate for defining dust. */
+//static const unsigned int CORE_DUST_RELAY_TX_FEE = 3000;
+
+static const int32_t CORE_CURRENT_TX_VERSION=2;
+static const int32_t CORE_MAX_STANDARD_TX_VERSION=2;
+
+CAmount CoreGetDustThreshold(const CTxOut& txout, const CFeeRate& dustRelayFee);
+bool CoreIsDust(const CTxOut& txout, const CFeeRate& dustRelayFee);
+bool CoreIsStandard(const CScript& scriptPubKey, txnouttype& whichType);
+bool CoreIsStandardTx(const CTransaction& tx, bool permit_bare_multisig, const CFeeRate& dust_relay_fee, std::string& reason);
+
+#endif /* BITCOIN_CORE_POLICY */
