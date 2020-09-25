3dd9380ed29aaf46c973d70ecb77a189df2fa7a6
CryptAxe <cryptaxe@gmail.com>
2020-09-25 13:03:08 -0700

    Special case for fixed mainchain bug
    * Ignore invalid WT^ updates before block 2921

--- src/validation.cpp
+++ src/validation.cpp
@@ -2314,7 +2314,10 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
                         client.HaveFailedWTPrime(hashWTPrime) :
                         client.HaveSpentWTPrime(hashWTPrime);
 
-                    if (!fVerified)
+
+                    // TODO remove case for height < 2921
+                    // Special case for bug fixed during drivechain testnet 34
+                    if (!fVerified && pindex->nHeight > 2921)
                         return state.Error(strprintf("%s: Invalid WT^ update : %s - %s!\n",
                                     __func__, fFailCommit ? "Failed" : "Paid out",
                                     hashWTPrime.ToString()));
