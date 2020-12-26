bc7005820f6c677345ea9d2595d4dd1bbafdb0da
cryptaxe <cryptaxe@gmail.com>
2020-12-26 15:58:28 -0800

    Update deposit address test

--- src/test/sidechain_tests.cpp
+++ src/test/sidechain_tests.cpp
@@ -794,7 +794,7 @@ BOOST_AUTO_TEST_CASE(depositaddress)
 
     std::string strDepositAddress = GenerateDepositAddress(strDest);
 
-    BOOST_CHECK(strDepositAddress == "s0_patrick_643e86");
+    BOOST_CHECK(strDepositAddress == "s1_patrick_1c0390");
 
     // Parsing should return just "patrick"
     std::string strOut = "";
