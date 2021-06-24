b92f6faa453fbb31c67656c605a54016c6955ce6
cryptaxe <cryptaxe@gmail.com>
2021-06-23 18:04:03 -0700

    New genesis block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -99,7 +99,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x14d9aa5f9fbaa70eb702a64e5e9d1684ad38bf31737c17bd19b5b49705444774");
+        consensus.defaultAssumeValid = uint256S("0xf157ef858e0ccfba417a5759cd6a5b330dbe73942f6ea41012205ac7f7799195");
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -113,10 +113,10 @@ public:
         nDefaultPort = 2751;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1624147757, 1, 0);
+        genesis = CreateGenesisBlock(1624496323, 1, 0);
         consensus.hashGenesisBlock = genesis.GetHash();
 
-        assert(consensus.hashGenesisBlock == uint256S("0x14d9aa5f9fbaa70eb702a64e5e9d1684ad38bf31737c17bd19b5b49705444774"));
+        assert(consensus.hashGenesisBlock == uint256S("0xf157ef858e0ccfba417a5759cd6a5b330dbe73942f6ea41012205ac7f7799195"));
         assert(genesis.hashMerkleRoot == uint256S("0x8eb1364f43885edf1322b2d32095e57abb03c32a61a80ac25c8db3de58e16b8a"));
 
         vSeeds.clear();
@@ -140,7 +140,7 @@ public:
 
         checkpointData = {
             {
-                { 0, uint256S("0x14d9aa5f9fbaa70eb702a64e5e9d1684ad38bf31737c17bd19b5b49705444774")},
+                { 0, uint256S("0xf157ef858e0ccfba417a5759cd6a5b330dbe73942f6ea41012205ac7f7799195")},
             }
         };
 
