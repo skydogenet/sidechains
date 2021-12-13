e0a6423272ccfb921dfd0e08e855f54be26d789a
cryptaxe <cryptaxe@gmail.com>
2021-12-12 16:20:13 -0800

    Update SidechainClient::SendBMMRequest, remove prevBlockRef number

--- src/sidechainclient.cpp
+++ src/sidechainclient.cpp
@@ -274,8 +274,6 @@ uint256 SidechainClient::SendBMMRequest(const uint256& hashCritical, const uint2
     json.append(hashCritical.ToString());
     json.append("\",");
     json.append(UniValue((int)THIS_SIDECHAIN).write());
-    json.append(",");
-    json.append(UniValue(0).write());
     json.append(",\"");
     json.append(strPrevHash.substr(strPrevHash.size() - 4, strPrevHash.size() - 1));
     json.append("\"");
